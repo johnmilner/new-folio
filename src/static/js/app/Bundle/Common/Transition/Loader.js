485994456116199b74b72390574dc755d5342c87
johnmilner <john.milner@mac.com>
2018-06-21 15:45:20 -0700

    work on mise-en-scene for preloading animation with slight delay and header reveal, pt.B

--- src/static/js/app/Bundle/Common/Transition/Loader.js
+++ src/static/js/app/Bundle/Common/Transition/Loader.js
@@ -12,9 +12,9 @@ const intro = function() {
   tl.from({el: '.header', p: {opacity: [0, 1], scale: [1.1, 1]}, d: 250, e: 'ExpoIn'})
   //tl.from({el: '.tagline', p: {y: [100, 0]}, d: 1600, e: 'Power4InOut'})
   tl.from({el: '.scroll-icon-wrap', p: {opacity: [0, 1], y: [100, 0]}, d: 1500, e: 'Power4InOut', delay: 500})
-  tl.from({el: '#burger-border-wrap', p: {opacity: [0, .6]}, d: 1500, e: 'ExpoOut', delay: 500})
-  tl.from({el: '.burger-line-hover', p: {x: [105, 0]}, d: 1000, e: 'ExpoOut', delay: 750})
-  tl.from({el: '#burger-mask', p: {y: [100, -100]}, d: 2000, e: 'ExpoOut', delay: 1000})
+  tl.from({el: '#burger-border-wrap', p: {opacity: [0, .6]}, d: 1500, e: 'ExpoOut', delay: 200})
+  tl.from({el: '.burger-line-hover', p: {x: [105, 0]}, d: 1000, e: 'ExpoOut', delay: 500})
+  tl.from({el: '#burger-mask', p: {y: [100, -100]}, d: 2000, e: 'ExpoOut', delay: 750})
 
   tl.play()
 }
@@ -26,7 +26,7 @@ Loader.pr = new S.Timeline()
 const isObj = S.Is.object(Loader.pr)
 const t = -1
 
-Loader.pr.from({el: ".loader-line", p: { y: [100, 100 * t] }, d: 4000, e: "Power4InOut"});
+Loader.pr.from({el: ".loader-line", p: { y: [100, 100 * t] }, d: 4000, e: "ExpoOut", delay: 600});
 
 
 Loader.run = function() {
