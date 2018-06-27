e82f0c8cdb42601abd66d1e2b70910786c4bedbe
johnmilner <john.milner@mac.com>
2018-06-26 20:28:56 -0700

    reposition h-client-wrap rotating 90deg in upperleft, pt.A

--- src/app/View/page/home.php
+++ src/app/View/page/home.php
@@ -241,7 +241,7 @@
                     <div id="h-btn-cover-3" class="h-btn-cover"></div>
                 </div>
             </div>
-            <div id="h-img-wrap-0" class="h-img-wrap"><img id="h-img-0" class="h-img" src="/static/media/img/home/case/img-0.jpg" alt=""> <img id="h-img-0-b" class="h-img" src="/static/media/img/home/case/img-0-b.png" alt=""></div>
+            <div id="h-img-wrap-0" class="h-img-wrap"><img id="h-img-0" class="h-img" src="/static/media/img/home/case/surf.png" alt=""> <img id="h-img-0-b" class="h-img" src="/static/media/img/home/case/img-0-b.png" alt=""></div>
             <div id="h-img-wrap-1" class="h-img-wrap"><img id="h-img-1" class="h-img" src="/static/media/img/home/case/img-1.jpg" alt=""></div>
             <div id="h-img-wrap-2" class="h-img-wrap"><img id="h-img-2" class="h-img" src="/static/media/img/home/case/img-2.jpg" alt=""></div>
             <div id="h-img-wrap-3" class="h-img-wrap"><img id="h-img-3" class="h-img" src="/static/media/img/home/case/img-3.jpg" alt=""></div>
